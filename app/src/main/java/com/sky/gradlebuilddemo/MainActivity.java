45be70da3e523957e6e244ef9859712cfd584442
shenbibo <shenbb@fsmeeting.com>
2016-10-24 17:14:16 +0800

    添加Flavor Dimensions demo，支持将一些公共的属性，资源提出到一个特定的dimension 中，减少重复的代码

--- app/src/main/java/com/sky/gradlebuilddemo/MainActivity.java
+++ app/src/main/java/com/sky/gradlebuilddemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sky.gradlebuilddemo;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +22,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 MainActivity.this.startActivity(new Intent(MainActivity.this, HelpTestActivity.class));
+            }
+        });
+
+        Button cancelButton = (Button) findViewById(R.id.cancel_button);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tips = MainActivity.this.getResources().getString(R.string.cancel_tips_msg);
+                String finalTips = String.format(tips, ((Button)v).getText());
+                Snackbar.make(MainActivity.this.getWindow().getDecorView(), finalTips, Snackbar
+                        .LENGTH_LONG).show();
             }
         });
     }
